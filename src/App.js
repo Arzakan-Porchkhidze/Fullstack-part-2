3bfb95912041e319ac94905847ffb5e44be61023
Arzakan <arzakanporchkhidze17@mgail.com>
2020-09-25 22:03:03 +0400

    data is fetched from the server using the axios-library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddName from './components/AddName'
 import Persons from './components/persons'
 import Search from './components/Search'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]) 
+    const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ search, setSearch] = useState('')
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
   
   const handleChange = (e) =>{
         setNewName(e.target.value) 
